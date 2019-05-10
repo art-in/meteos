067f59da553bd67954452aa96c01413a22520da4
artin-phares <artin@artin.space>
2019-05-10 15:52:47 +0300

    #18 add eslint rules for react hooks

--- frontend/src/client/client.js
+++ frontend/src/client/client.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('#root')
+);
